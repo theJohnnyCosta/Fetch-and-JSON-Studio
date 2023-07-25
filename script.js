692be0df7c09b4d80c5a098af9d20efe41866864
Johnny Costa <johnnycosta@johnnys-air.mynetworksettings.com>
2023-07-24 20:58:02 -0400

    finished studio

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    const container = document.getElementById("container");
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+               response.json().then(function (data) {
+                console.log(data);
+                for (let i = 0; i < data.length; i++) {
+                    container.innerHTML += 
+                `<div class="astronaut">
+                    <div class="bio">
+                       <h3>${data[i].firstName} ${data[i].lastName}</h3>
+                       <ul>
+                          <li>Hours in space: ${data[i].hoursInSpace}</li>
+                          <li>Active: ${data[i].active}</li>
+                          <li>Skills: ${data[i].skills}</li>
+                       </ul>
+                    </div>
+                    <img class="avatar" src="${data[i].picture}">
+                 </div>` 
+                };
+               });
+  });
+});
